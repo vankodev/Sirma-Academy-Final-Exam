ecda2d4a9b8e088402fb456c41c84d77459923dc
Ivan Ivanov <vankodev@gmail.com>
2024-09-10 01:23:41 +0300

    Render Team Component Data

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,5 +1,39 @@
+import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import DataContext from '../contexts/dataContext';
+import styles from './Team.module.css';
+
 function Team() {
-    return <h1>Team View</h1>;
+    const { teamID } = useParams();
+    const { teams, players } = useContext(DataContext);
+
+    if (!teams || !players) {
+        return <h3>Loading...</h3>;
+    }
+
+    const team = teams.find((team) => team.ID === teamID);
+    const teamPlayers = players.filter((player) => player.TeamID === teamID);
+
+    return (
+        <div className='team'>
+            <h1>{team.Name}</h1>;<h2>Group: {team.Group}</h2>
+            <h4>Manager: {team.ManagerFullName}</h4>
+            <table className={styles.playersTable}>
+                <tr>
+                    <th>Number</th>
+                    <th>Player</th>
+                    <th>Position</th>
+                </tr>
+                {teamPlayers.map((player) => (
+                    <tr key={player.ID}>
+                        <td>{player.TeamNumber}</td>
+                        <td>{player.FullName}</td>
+                        <td>{player.Position}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
 }
 
 export default Team;
