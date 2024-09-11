087b2a75fe660886b02c5a83fdc7f0f05a337494
Ivan Ivanov <vankodev@gmail.com>
2024-09-11 23:52:43 +0300

    Fix Small Styling Issue

--- src/components/GroupStage.jsx
+++ src/components/GroupStage.jsx
@@ -13,19 +13,17 @@ function GroupStage({ matches }) {
     }, {});
 
     return (
-        <div className='container'>
-            <div className={styles.groupStage}>
-                {Object.entries(matchesByDate).map(([date, dateMatches]) => (
-                    <React.Fragment key={date}>
-                        <h4 className={styles.date}>{date}</h4>
-                        <div className={styles.matches}>
-                            {dateMatches.map((match) => (
-                                <MatchCard key={match.ID} match={match} />
-                            ))}
-                        </div>
-                    </React.Fragment>
-                ))}
-            </div>
+        <div className={styles.groupStage}>
+            {Object.entries(matchesByDate).map(([date, dateMatches]) => (
+                <React.Fragment key={date}>
+                    <h4 className={styles.date}>{date}</h4>
+                    <div className={styles.matches}>
+                        {dateMatches.map((match) => (
+                            <MatchCard key={match.ID} match={match} />
+                        ))}
+                    </div>
+                </React.Fragment>
+            ))}
         </div>
     );
 }
